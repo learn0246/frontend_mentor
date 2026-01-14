6a8843b88ba701a2595c5b63cfaa1421116668ce
learn0246 <learn0246@gmail.com>
2026-01-15 00:37:36 +0800

    Completed digitalbank-landing-page

---
+++ development/digitalbank-landing-page/scripts/script.js
@@ -0,0 +1,37 @@
+const dropdownMenu = document.querySelector(".js-site-header__dropdown-menu");
+const toggleButton = document.querySelector(".js-button-toggle");
+const toggleButtonIcon = document.querySelector(".js-button-toggle .fa-bars");
+const overlay = document.querySelector(".overlay");
+
+overlay.style.display = "none";
+
+function toggleDropdownMenu() {
+  toggleOverlay();
+  dropdownMenu.classList.toggle("show");
+
+  const isMenuShown = dropdownMenu.classList.contains("show");
+  toggleButtonIcon.classList = isMenuShown
+    ? "fa-solid fa-xmark"
+    : "fa-solid fa-bars";
+}
+
+function toggleOverlay() {
+  const overlayState = overlay.style.display;
+  overlay.style.display = overlayState == "none" ? "block" : "none";
+}
+
+toggleButton.onclick = toggleDropdownMenu;
+overlay.onclick = toggleDropdownMenu;
+
+let prevScrollPos = 0;
+
+window.onscroll = function scroll() {
+  const currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > prevScrollPos) {
+    document.querySelector(".scroll-up").style.top = "-5rem";
+  } else {
+    document.querySelector(".scroll-up").style.top = "0";
+  }
+
+  prevScrollPos = currentScrollPos;
+};
